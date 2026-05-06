afbdb15fa92683499867126492bc2c0705ba3536
LonelyReed <cxlxh220416@163.com>
2026-05-06 20:10:16 +0800

    5.5打卡DAY 19

---
+++ 2026年春/5.5/A题/A题/Anyway Takahashi.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+	int a, b, c, d, result;
+	std::cin >> a >> b >> c >> d;
+
+	result = (a + b) * (c - d);
+
+	std::cout << result << std::endl;
+	std::cout << "Takahashi";
+
+	return 0;
+}
